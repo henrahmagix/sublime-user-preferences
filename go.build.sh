a95d858e57f35d4004d63ae46adb89598aac93bc
Henry Blyth <blyth.henry@gmail.com>
2018-08-06 15:57:29 +0100

    go build system update

--- go.build.sh
+++ go.build.sh
@@ -1,6 +1,9 @@
 set -x
 
-dep ensure && \
+dep ensure -v && \
 go install ./... && \
 go test ./... && \
+go vet ./... && \
+gosimple ./... && \
+find . -type d -not -path '*/vendor*' -not -path '*.git*' -exec gotype -t {} \; && \
 gometalinter.v2 --fast --deadline=300s --vendor --line-length=100 --enable=lll  --enable=gofmt --enable=staticcheck  ./...
