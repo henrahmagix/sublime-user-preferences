03615e48b166a5df6bd40406825ddbef7903909b
Henry Blyth <blyth.henry@gmail.com>
2018-09-17 10:39:20 +0100

    No more chaining in go build script
    SublimeText 3, some builds crash `plugin_host` when a build script exits > 0,
    which is what this chaining was for. Now using older Sublime build without bug.

--- go.build.sh
+++ go.build.sh
@@ -1,9 +1,12 @@
+#! /usr/bin/env bash
+
+set -e
 set -x
 
-(make deps || dep ensure) && \
-go install ./... && \
-go test ./... && \
-go vet ./... && \
-gosimple ./... && \
-find . -type d -not -path '*/vendor*' -not -path '*.git*' -exec gotype -t {} \; && \
+pgrep dep || dep ensure
+go install ./...
+go test ./...
+go vet ./...
+gosimple ./...
+find . -type d -not -path '*/vendor*' -not -path '*.git*' -exec gotype -t {} \;
 gometalinter.v2 --fast --deadline=300s --vendor --line-length=100 --enable=lll  --enable=gofmt --enable=staticcheck  ./...
